b3bb8f33eb15a39a61bd9744261269c598b408c2
David Mayou <davidpmayou@gmail.com>
2019-02-03 13:37:42 -0600

    add application route

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const applicationRouter = require('./routes/application.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/application', applicationRouter);
 
 // Serve static files
 app.use(express.static('build'));
