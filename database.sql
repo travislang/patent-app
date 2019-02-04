79b507c90734fbf4b151cf49e13c8da447a9d943
David Mayou <davidpmayou@gmail.com>
2019-02-04 13:26:36 -0600

    change tables based on updated scope

--- database.sql
+++ database.sql
@@ -2,8 +2,13 @@ CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "user_name" VARCHAR (20) UNIQUE NOT NULL,
     "password" VARCHAR (255) NOT NULL,
-    "role" VARCHAR(20) NOT NULL,
-    "is_admin" BOOLEAN NOT NULL DEFAULT FALSE,
+    "is_admin" BOOLEAN DEFAULT FALSE,
+    "signature_name" VARCHAR(70),
+    "registration_number" VARCHAR(10),
+    "phone_number" VARCHAR(20),
+    "firm_name" VARCHAR(70),
+    "uspto_customer_number" VARCHAR(9),
+    "deposit_account_number" VARCHAR(9),
     "active" BOOLEAN DEFAULT TRUE
 );
 
@@ -20,50 +25,38 @@ CREATE TABLE "application" (
     "inactive" BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE "office_action" (
-    "id" SERIAL PRIMARY KEY,
-    "application_id" INTEGER REFERENCES "application",
-    "response_due_date" DATE,
-    "response_sent_date" DATE,
-    "status_id" INTEGER REFERENCES "status"
-);
-
 CREATE TABLE "status" (
     "id" SERIAL PRIMARY KEY,
     "status" VARCHAR(30),
     "color" VARCHAR(10)
 );
 
-CREATE TABLE "rejection_reason" (
+CREATE TABLE "office_action" (
     "id" SERIAL PRIMARY KEY,
-    "reason" VARCHAR(30)
+    "application_id" INTEGER REFERENCES "application",
+    "uspto_mailing_date" DATE,
+    "response_sent_date" DATE,
+    "status_id" INTEGER REFERENCES "status"
 );
 
-CREATE TABLE "strategy" (
+CREATE TABLE "template_type" (
     "id" SERIAL PRIMARY KEY,
-    "text" VARCHAR(100)    
+    "type" VARCHAR(30)
 );
 
 CREATE TABLE "template" (
     "id" SERIAL PRIMARY KEY,
-    "strategy_id" INTEGER REFERENCES "strategy",
-    "name" VARCHAR(60),
-    "content" VARCHAR(1000)
+    "template_name" VARCHAR(60),
+    "content" VARCHAR(1000),
+    "user_id" INTEGER REFERENCES "user"
 );
 
 CREATE TABLE "issue" (
     "id" SERIAL PRIMARY KEY,
     "office_action_id" INTEGER REFERENCES "office_action",
-    "rejection_reason_id" INTEGER REFERENCES "rejection_reason",
+    "template_type_id" INTEGER REFERENCES "template_type",
     "claims" VARCHAR(30),
-    "need_peer_approval" BOOLEAN DEFAULT FALSE,
-    "need_client_approval" BOOLEAN DEFAULT FALSE,
-    "peer_approved" BOOLEAN DEFAULT FALSE,
-    "client_approved" BOOLEAN DEFAULT FALSE,
-    "template_id" INTEGER REFERENCES "template",
-    "comment" VARCHAR(255),
-    "comment_peer_review" VARCHAR(255),
-    "comment_client_review" VARCHAR(255)
+    "template_id" INTEGER REFERENCES "template"
 );
 
 CREATE TABLE "field_code" (
