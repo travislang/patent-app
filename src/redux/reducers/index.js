dba1f866daab204932397d1662c4ee7ac9b4ec37
Josh Byron <joshhbyron@gmail.com>
2019-02-05 13:38:12 -0600

    Very minor fixes

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,6 +3,12 @@ import errors from './errorsReducer';
 import loginMode from './loginModeReducer';
 import user from './userReducer';
 
+// *----------* Patent app *----------*
+import applicationList from './applicationListReducer'
+import application from './applicationReducer'
+import template from '../reducers/templateReducer'
+
+
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
 // This is imported in index.js as rootSaga
@@ -13,6 +19,9 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   loginMode, // will have a value of 'login' or 'registration' to control which screen is shown
   user, // will have an id and username if someone is logged in
+  applicationList, // Reducer responsible for providing access to all applications 
+  application, // Reducer responsible for providing access to all applications
+  template, // Reducer responsible for providing access to all templates
 });
 
 export default rootReducer;
