114f527f54be1b330a16e8a6f2d450f240e1e11a
phiathao <phiat11@gmail.com>
2019-02-06 10:22:21 -0600

    wip filer id

--- server/routes/uspto.router.js
+++ server/routes/uspto.router.js
@@ -8,12 +8,13 @@ const axios = require('axios');
 
 // GET ROUTE
 router.get('/', rejectUnauthenticated, (req, res) => {
+    const filterId = req.body.applId
 
     // ---- USING USPTO API  ---- 
     axios.post( // USPTO use POST route to grab information
         'https://ped.uspto.gov/api/queries', // api route
         {
-            "searchText": `applId:${req.body.applId}`, // search for this information / applId is the application id
+            "searchText": `applId:${filterId}`, // search for this information / applId is the application id
             "qf": "applId", // where it is searching, in this case just the application Id of the patent
         }
         // {
