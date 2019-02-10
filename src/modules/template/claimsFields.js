b322417f208b9ab200a55fca400399db720c37fc
David Mayou <davidpmayou@gmail.com>
2019-02-09 19:09:31 -0600

    add claimOrClaims

--- src/modules/template/claimsFields.js
+++ src/modules/template/claimsFields.js
@@ -1,6 +1,25 @@
+const claimOrClaims = (claims) => {
+    claims = claims || '';
+    if (claimsArePlural(claims)) {
+        return `s ${claims}`;
+    } else {
+        return ` ${claims}`;
+    }
+};
+
+const claimIsAre = (claims) => {
+    if (claimsArePlural(claims)) {
+        return `s ${claims} are`;
+    } else {
+        return ` ${claims} is`;
+    }
+};
+
 const claimsArePlural = (claims) => {
     // considered plural if any commas or dashes are present
     return /[,;:-]/.test(claims);
-}
+};
 
-export default claimsArePlural;
+export {claimsArePlural};
+export {claimIsAre};
+export {claimOrClaims};
--- src/modules/test/claimsFields.test.js
+++ src/modules/test/claimsFields.test.js
@@ -1,11 +1,11 @@
-import claimsArePlural from '../template/claimsFields';
+import { claimsArePlural, claimOrClaims, claimIsAre } from '../template/claimsFields';
 
 const testCases = [
-        { description: 'Single-digit claim is singular.', claim: '1', expectedResult: false },
-        { description: 'Multi-digit claim is singluar', claim: '10', expectedResult: false },
-        { description: 'Claim range is plural', claim: '1-5', expectedResult: true },
-        { description: 'Empty claim returns false (arbitrary)', claim: '', expectedResult: false },
-        { description: 'Null claim returns false (arbitrary)', claim: null, expectedResult: false },
+        { description: 'Single-digit claim is singular.', claim: '1', expectedResult: false, claimS: ' 1' },
+        { description: 'Multi-digit claim is singluar', claim: '10', expectedResult: false, claimS: ' 10' },
+        { description: 'Claim range is plural', claim: '1-5', expectedResult: true, claimS: 's 1-5' },
+        { description: 'Empty claim returns false (arbitrary)', claim: '', expectedResult: false, claimS: ' ' },
+        { description: 'Null claim returns false (arbitrary)', claim: null, expectedResult: false, claimS: ' ' },
         // { description: '', claim: , expectedResult:  },
 ]
 
@@ -13,5 +13,12 @@ for (let testCase of testCases) {
     test(`${testCase.description}`, () => {
         expect(claimsArePlural(testCase.claim))
             .toBe(testCase.expectedResult);
+    });
+}
+
+for(let testCase of testCases) {
+    test(`${testCase.description}`, () => {
+        expect(claimOrClaims(testCase.claim))
+            .toBe(testCase.claimS);
     });
 }
