0957f9a7012616d3351b7d1ac401719be936417b
David Mayou <davidpmayou@gmail.com>
2019-02-10 16:37:03 -0600

    stub replace claims fields function

--- src/modules/template/replaceTemplateFields.js
+++ src/modules/template/replaceTemplateFields.js
@@ -1,8 +1,28 @@
 import fieldCodes from './legalCodes';
-const { legalCodes, keys, defaultValue } = fieldCodes;
+const { legalCodes, keys, defaultValue, claimCodes } = fieldCodes;
 
 const replaceTemplateFields = (template, values) => {
     // assumes valid field codes and template syntax
+    let temp = '';
+    for (let i = 0; i < legalCodes.length; i++) {
+        let reg = new RegExp(`{${legalCodes[i]}}`, 'g');
+        let replacement = values[keys[i]] ? values[keys[i]] : defaultValue;
+        template = template.replace(reg, replacement);
+    }
+    return template;
+};
+
+const replaceClaimsFields = (template, claims) => {
+    let temp = '';
+    for (let i = 0; i < claimCodes.length; i++) {
+        let reg = new RegExp(`{${claimCodes[i]}}`, 'g');
+        let replacement = values[keys[i]] ? values[keys[i]] : defaultValue;
+        template = template.replace(reg, replacement);
+    }
+    return template;
+};
+
+const replaceNonClaimsFields = (template, values) => {
     let temp = '';
     for (let i = 0; i < legalCodes.length; i++) {
         let reg = new RegExp(`{${legalCodes[i]}}`, 'g');
