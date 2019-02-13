a9881cc558b90196f801ac02178b139827cfce54
David Mayou <davidpmayou@gmail.com>
2019-02-13 10:08:34 -0600

    add SET_ALL_TEMPLATES reducer

--- src/redux/reducers/templateReducer.js
+++ src/redux/reducers/templateReducer.js
@@ -1,10 +1,12 @@
 // Reducer responsible for providing access to all templates
-const templateReducer = (state = { templates:[], types: []}, action) => {
+const templateReducer = (state = { templates:[], types: [], allTemplates: [] }, action) => {
     switch (action.type) {
         case 'SET_TEMPLATES':
-            return {... state, templates: action.payload};
+            return {...state, templates: action.payload};
         case 'SET_TEMPLATE_TYPES':
-            return {... state, types: action.payload};
+            return {...state, types: action.payload};
+        case 'SET_ALL_TEMPLATES;':
+            return {...state, allTemplates: action.payload};
         default:
             return state;
     }
