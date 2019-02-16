2a1072a028b00ef44c14ff011a8c5a89b5621307
David Mayou <davidpmayou@gmail.com>
2019-02-16 17:42:54 -0600

    add verifyTemplate test cases

--- src/modules/test/verifyTemplate.test.js
+++ src/modules/test/verifyTemplate.test.js
@@ -6,6 +6,8 @@ const testTemplate = [
     { description: 'Empty template returns false', template: '', expectedResult: false },
     { description: 'Brackets in matching pairs and legal fields return true', template: 'Hello world-these are legal field names: {inventorName} hello world {applNumber}.' , expectedResult: true },
     { description: 'Templates without brackets return true', template: 'Hello world no brackets.' , expectedResult: true },
+    { description: 'Extra closing bracket returns false', template: 'First some text and a {applicantName} } and more text', expectedResult: false },
+    { description: 'Extra closing bracket returns false', template: 'First a closing } bracket and a {applicantName} and more text', expectedResult: false },
     { description: 'Nested brackets not allowed', template: 'Hello world {field {field} }', expectedResult: false },
     { description: 'Illegal fields not allowed', template: 'This is not a legal field name: {notafield}', expectedResult: false },
     // { description: '', template: , expectedResult:  },
